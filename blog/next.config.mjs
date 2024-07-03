7a9b147f831dccdcfbaf9f6eee62d27ceb24ef3e
Duong Bao Duy <baoduy.duong0206@gmail.com>
2024-07-03 22:26:00 +0700

    load svg image as tsx module

--- blog/next.config.mjs
+++ blog/next.config.mjs
@@ -14,6 +14,51 @@ const nextConfig = {
         contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     },
     reactStrictMode: true,
+    webpack(config) {
+        // Grab the existing rule that handles SVG imports
+        const fileLoaderRule = config.module.rules.find((rule) =>
+            rule.test?.test?.('.svg'),
+        )
+
+        config.module.rules.push(
+            // {
+            //     test: /\.svg$/,
+            //     issuer: { and: [/\.(js|ts|md)x?$/] },
+            //     use: [
+            //         {
+            //             loader: '@svgr/webpack',
+            //             options: {
+            //                 icon: true,
+            //                 svgoConfig: { plugins: [{ removeViewBox: false }] }
+            //             }
+            //         },
+            //         "url-loader"
+            //     ],
+            // },
+            // Reapply the existing rule, but only for svg imports ending in ?url
+            {
+                ...fileLoaderRule,
+                test: /\.svg$/i,
+                resourceQuery: /url/, // *.svg?url
+            },
+            // Convert all other *.svg imports to React components
+            {
+                test: /\.svg$/i,
+                issuer: /\.([jt]s|md)x?$/,
+                resourceQuery: { not: /url/ }, // exclude if *.svg?url
+                use: ['@svgr/webpack', 'url-loader'],
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader',
+            },
+        );
+
+        // Modify the file loader rule to ignore *.svg, since we have it handled now.
+        fileLoaderRule.exclude = /\.svg$/i
+
+        return config;
+    },
 };
 
 export default withMDX(nextConfig);
