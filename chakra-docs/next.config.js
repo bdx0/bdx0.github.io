df578bf999280d8f5f5d8db4d3818b49933a168f
Duong Bao Duy <baoduy.duong0206@gmail.com>
2024-07-06 11:47:27 +0700

    add chakra-docs

---
+++ chakra-docs/next.config.js
@@ -0,0 +1,26 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { withContentlayer } = require('next-contentlayer')
+
+module.exports = withContentlayer({
+  output: 'export',
+  optimizeFonts: true,
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      { hostname: 'img.youtube.com' },
+      { hostname: 'res.cloudinary.com' },
+      { hostname: 'github.com' },
+      { hostname: 'avatars.githubusercontent.com' },
+      { hostname: 'avatars0.githubusercontent.com' },
+      { hostname: 'avatars1.githubusercontent.com' },
+      { hostname: 'avatars2.githubusercontent.com' },
+      { hostname: 'avatars3.githubusercontent.com' },
+    ],
+  },
+  productionBrowserSourceMaps: true,
+  // redirects: require('./next-redirect'),
+  reactStrictMode: true,
+  experimental: {
+    externalDir: true,
+  },
+})
